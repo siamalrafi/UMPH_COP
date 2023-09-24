22ef6d234179d45a695b251d176113cdde14fdbe
siamalrafi <rahathasandipto@gmail.com>
2023-09-24 19:20:28 +0600

    new

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -35,7 +35,7 @@ const logger = createLogger({
   ],
 });
 
-const errorlogger = createLogger({
+const errorLogger = createLogger({
   level: 'error',
   format: combine(label({ label: 'PH' }), timestamp(), myFormat),
   transports: [
@@ -56,4 +56,4 @@ const errorlogger = createLogger({
   ],
 });
 
-export { logger, errorlogger };
+export { errorLogger, logger };
